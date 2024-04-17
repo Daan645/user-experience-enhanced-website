3417f577f02e67b16ff125f2977f374078e072b0
Daan645 <daan@videotechniek.nl>
2024-04-17 12:44:16 +0200

    menu and image standard sizing

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,29 +5,58 @@
 // document.addEventListener('DOMContentLoaded', function() {
 //     // pakt de data uit het inputveld van html en zet deze om in een js object onder de variabele itemsData
 //     var itemsData = JSON.parse(document.getElementById('itemsData').value);
+// nav
 
-    // Detail pagina
-    // Detail inklap/uitklap functie
-    const detailsButton = document.querySelector('.details-button');
-    const details = document.querySelector('.details');
-    let detailsCheck = false;
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
 
-    // Voeg de eventlistener toe aan de detailsknop
-    detailsButton.addEventListener('click', function() {
-        if (detailsCheck === false) {
-            detailsActive();
-        } else {
-            detailsNonActive();
-        }
-    });
 
-    // Functie voor het tonen van details
-    function detailsActive() {
-        detailsCheck = true;
-        details.classList.add('details-active');
-        detailsButton.style.borderBottomLeftRadius = '0';
-        detailsButton.style.borderBottomRightRadius = '0';
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+
     }
 });
+
+
+// Detail pagina
+// Detail inklap/uitklap functie
+const detailsButton = document.querySelector('.details-button');
+const details = document.querySelector('.details');
+let detailsCheck = false;
+
+// Voeg de eventlistener toe aan de detailsknop
+detailsButton.addEventListener('click', function() {
+    if (detailsCheck === false) {
+        detailsActive();
+    } else {
+        detailsNonActive();
+    }
+});
+
+// Functie voor het tonen van details
+function detailsActive() {
+    detailsCheck = true;
+    details.classList.add('details-active');
+    detailsButton.style.borderBottomLeftRadius = '0';
+    detailsButton.style.borderBottomRightRadius = '0';
+}
+
 
 
